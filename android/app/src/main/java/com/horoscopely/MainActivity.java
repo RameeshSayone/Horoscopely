5145be59754556885bd37c079f73b5970b6d9bc2
rameesh <rameesh@sayonetech.com>
2023-11-13 14:28:02 +0530

    added splash screen and logo in android

--- android/app/src/main/java/com/horoscopely/MainActivity.java
+++ android/app/src/main/java/com/horoscopely/MainActivity.java
@@ -4,13 +4,19 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-
+import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+   @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this, R.style.BootTheme); // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
+  }
   @Override
   protected String getMainComponentName() {
     return "Horoscopely";
